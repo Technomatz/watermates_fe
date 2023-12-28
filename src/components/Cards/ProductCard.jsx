934ace2c71b04a91fa6c6a5754cdb52154de5cdf
Devendra Yadav <dyadav@technomatz.com>
2023-12-28 18:49:32 +0530

    remove code smell

--- src/components/Cards/ProductCard.jsx
+++ src/components/Cards/ProductCard.jsx
@@ -1,30 +1,10 @@
 import React from 'react';
-// import { Card, CardContent, CardMedia, IconButton } from '@mui/material';
-// import FavoriteIcon from '@mui/icons-material/Favorite';
 import { Link } from 'react-router-dom';
 import './ProductCard.css';
 
 function ProductCard({ imgUrl, title, discription, price }) {
   return (
     <Link to={`/product/${1}`} style={{ textDecoration: 'none' }}>
-      {/* <Card classNameName="cardHead">
-        <CardMedia
-          component="img"
-          classNameName="cardImg"
-          height="300"
-          alt="Big Image"
-          src={imgUrl}
-        />
-        <IconButton classNameName="favIcon" color="primary" aria-label="favorite">
-          <FavoriteIcon />
-        </IconButton>
-
-        <CardContent>
-          <h2 classNameName="textUpper">{title}</h2>
-          <p classNameName="textUpper">{discription}</p>
-          <h1>₹ {price}/-</h1>
-        </CardContent>
-      </Card> */}
       <div className="relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
         <div className="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white bg-clip-border rounded-xl h-96">
           <img
