4dc1dde25a803ed76e542ffcddbf01c433e38de1
Sanjil Kaushal <skaushal@technomatz.com>
2024-01-12 11:24:24 +0530

    Implemented formik validations into login and signup forms

--- src/components/SignUp/Index.jsx
+++ src/components/SignUp/Index.jsx
@@ -12,30 +12,64 @@ import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const SignupSchema = Yup.object().shape({
+  full_name: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+  phone_number: Yup.string()
+    .min(10, 'Phone number is invalid')
+    .required('Required'),
+  password: Yup.string().min(8, 'Too Short!').required('Required'),
+});
 
 export default function SignUp() {
   const navigate = useNavigate();
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    const finalData = {
-      user: {
-        email: data.get('email'),
-        full_name: data.get('full_name'),
-        phone_number: data.get('phone_number'),
-        password: data.get('password'),
-      },
-    };
-    post('/users/sign_up', finalData)
-      .then((res) => {
-        localStorage.setItem('token', res.data.token);
-        navigate('/login');
-      })
-      .catch((err) => alert(err));
-  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      full_name: '',
+      phone_number: '',
+      password: '',
+    },
+    validationSchema: SignupSchema,
+    onSubmit: (values) => {
+      const finalData = {
+        user: {
+          email: values.email,
+          full_name: values.full_name,
+          phone_number: values.phone_number,
+          password: values.password,
+        },
+      };
+      post('/users/sign_up', finalData)
+        .then((res) => {
+          localStorage.setItem('token', res.data.token);
+          toast.success('Success Notification !', {
+            position: toast.POSITION.TOP_RIGHT,
+          });
+          setTimeout(() => {
+            navigate('/');
+          }, 3000);
+        })
+        .catch((err) =>
+          toast.error(err.response.data.errors[0], {
+            position: toast.POSITION.TOP_RIGHT,
+          }),
+        );
+    },
+  });
 
   return (
     <Grid container component="main" sx={{ height: '100vh' }}>
+      <ToastContainer />
       <CssBaseline />
       <Grid
         item
@@ -64,60 +98,75 @@ export default function SignUp() {
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 1 }}
-          >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="full_name"
-              label="Full Name"
-              name="full_name"
-              autoComplete="full_name"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="phone_number"
-              label="Phone Number"
-              name="phone_number"
-              autoComplete="phone_number"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
+          <Box sx={{ mt: 1 }}>
+            <form onSubmit={formik.handleSubmit}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="full_name"
+                label="Full Name"
+                name="full_name"
+                value={formik.values.full_name}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.full_name && Boolean(formik.errors.full_name)
+                }
+                helperText={formik.touched.full_name && formik.errors.full_name}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="phone_number"
+                label="Phone Number"
+                name="phone_number"
+                value={formik.values.phone_number}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.phone_number &&
+                  Boolean(formik.errors.phone_number)
+                }
+                helperText={
+                  formik.touched.phone_number && formik.errors.phone_number
+                }
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+                helperText={formik.touched.password && formik.errors.password}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+            </form>
             <Grid container>
               <Grid item>
                 <Link href="/login" variant="body2">
